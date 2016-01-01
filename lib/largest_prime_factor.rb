66766869deccf7aba1c0f692a8bd10d13a204553
Nathaniel Miller <dukeoflaser@gmail.com>
2016-01-01 22:24:33 +0000

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,21 @@
 # Enter your procedural solution here!
+require 'prime'
+
+def largest_prime_factor(input)
+
+  max_prime = 2
+  
+  Prime.each do |prime|
+    if input % prime == 0
+      if (input / prime).prime?
+        max_prime = input/prime
+        break
+      else
+        input /= prime
+      end 
+    end
+  end
+  
+  max_prime
+  
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,25 @@
 # Enter your object-oriented solution here!
+require 'prime'
+
+class LargestPrimeFactor
+  def initialize(input)
+    @number = input
+  end
+  
+  def number
+    max_prime = 2
+    
+    Prime.each do |prime|
+      if @number % prime == 0
+        if (@number / prime).prime?
+          max_prime = @number / prime
+          break
+        else
+          @number /= prime
+        end 
+      end
+    end
+    
+    max_prime
+  end
+end
