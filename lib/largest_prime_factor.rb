82ef148448a787a0ea55fe8470040df0c3e81c17
Nathaniel Miller <dukeoflaser@gmail.com>
2016-01-03 19:51:17 +0000

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -5,7 +5,8 @@ def largest_prime_factor(input)
 
   max_prime = 2
   
-  Prime.each do |prime|
+  #Prime.each do |prime|            -- to use Ruby's built in Prime class
+  first_primes.each do |prime|
     if input % prime == 0
       if (input / prime).prime?
         max_prime = input/prime
@@ -18,4 +19,18 @@ def largest_prime_factor(input)
   
   max_prime
   
+end
+
+def first_primes
+  max = 10000        #A bit arbitrary and fails on larger numbers
+  numbers = (2..max).to_a
+  upper = Math.sqrt(max)
+  
+  (2..upper).each do |n|
+    unless numbers[n - 2] == nil
+      (n * n).step(max, n) { |i| numbers[i - 2] = nil }
+    end
+  end
+
+  numbers.compact
 end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -9,7 +9,8 @@ class LargestPrimeFactor
   def number
     max_prime = 2
     
-    Prime.each do |prime|
+    #Prime.each do |prime|            -- to use Ruby's built in Prime class
+    first_primes.each do |prime|
       if @number % prime == 0
         if (@number / prime).prime?
           max_prime = @number / prime
@@ -22,4 +23,19 @@ class LargestPrimeFactor
     
     max_prime
   end
+  
+  def first_primes
+    max = 10000        #A bit arbitrary and fails on larger numbers
+    numbers = (2..max).to_a
+    upper = Math.sqrt(max)
+    
+    (2..upper).each do |n|
+      unless numbers[n - 2] == nil
+        (n * n).step(max, n) { |i| numbers[i - 2] = nil }
+      end
+    end
+  
+    numbers.compact
+  end
+  
 end
